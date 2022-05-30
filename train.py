e4e385502504189977c7e038f57409e826a4edb2
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-05-30 11:37:44 +0200

    bayesian search

--- train.py
+++ train.py
@@ -17,6 +17,8 @@ from datasets import TrainDataset
 from utils import AverageMeter, calc_psnr
 import time
 
+NB_DATA = 4474
+
 def objective(trial):
     parser = argparse.ArgumentParser()
     parser.add_argument('--HR_dir', type=str,default = "../BPNN/data/ROI_trab")
@@ -37,6 +39,7 @@ def objective(trial):
     parser.add_argument('--n3', type=int,default = 190)
     parser.add_argument('--gpu_ids', type=list, default = [0,1])
     parser.add_argument('--NB_LABEL', type=int, default = 5)
+    paser.add_argument('--k_fold', type=
     args = parser.parse_args()
 
     args.outputs_dir = os.path.join(args.outputs_dir, 'BPNN_first_shot_x{}'.format(args.scale))
@@ -61,101 +64,114 @@ def objective(trial):
         {'params': model.last_part.parameters(), 'lr': args.lr * 0.1}
     ], lr=args.lr)
     
-    
+    for param in model_bpnn.parameters():
+        param.requires_grad = False
+    model_bpnn.eval()
+                       
+    #train_dataset = TrainDataset(args.HR_dir,args.LR_dir)
+    index = range(NB_DATA)
+    kf = KFold(n_splits = opt['k_fold'], shuffle=True)
+    kf.get_n_splits(index)
+                                 
+    for train_index, test_index in kf.split(index):
+        dataset = TrainDataset(args.HR_dir, args.LR_dir)
+        train_dataloader = DataLoader(dataset=dataset,
+                                      batch_size=args.batch_size,
+                                      shuffle=True,
+                                      sampler=train_index,
+                                      num_workers=args.num_workers,
+                                      pin_memory=True)
+        eval_dataloader = DataLoader(dataset=dataset, 
+                                     shuffle=True,
+                                     sampler=test_index,
+                                     batch_size=1,
+                                    num_workers=args.num_workers)    
 
-    train_dataset = TrainDataset(args.HR_dir,args.LR_dir)
-    [trainset,valset] = train_test_split(train_dataset,test_size = 0.2, random_state=1)
-    train_dataloader = DataLoader(dataset=trainset,
-                                  batch_size=args.batch_size,
-                                  shuffle=True,
-                                  num_workers=args.num_workers,
-                                  pin_memory=True)
-    eval_dataloader = DataLoader(dataset=valset, batch_size=1)    
+        best_weights = copy.deepcopy(model.state_dict())
+        best_epoch = 0
+        best_loss = 10
+        t_score = []
+        tr_score = []
+        tr_bpnn = []
+        t_bpnn = []
+        start = time.time()
+          
+        for epoch in range(args.num_epochs):
+            model.train()
+            epoch_losses = AverageMeter()
+            bpnn_loss = AverageMeter()
 
-    best_weights = copy.deepcopy(model.state_dict())
-    best_epoch = 0
-    best_loss = 10
-    t_score = []
-    tr_score = []
-    tr_bpnn = []
-    t_bpnn = []
-    start = time.time()
-    for epoch in range(args.num_epochs):
-        model.train()
-        epoch_losses = AverageMeter()
-        bpnn_loss = AverageMeter()
+            with tqdm(total=(len(train_dataset) - len(train_dataset) % args.batch_size), ncols=80) as t:
+                t.set_description('epoch: {}/{}'.format(epoch, args.num_epochs - 1))
 
-        with tqdm(total=(len(train_dataset) - len(train_dataset) % args.batch_size), ncols=80) as t:
-            t.set_description('epoch: {}/{}'.format(epoch, args.num_epochs - 1))
+                for data in train_dataloader:
+                    inputs, labels = data
+                    inputs = inputs.reshape(inputs.size(0),1,256,256)
+                    labels = labels.reshape(labels.size(0),1,512,512)
+                    inputs, labels= inputs.float(), labels.float()
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+                    preds = model(inputs)
+                    P_SR = model_bpnn(preds)
+                    P_HR = model_bpnn(labels)
 
-            for data in train_dataloader:
+                    L_SR = criterion(preds, labels)
+                    L_BPNN = Lbpnn(P_HR,P_SR)
+                    loss = L_SR + (args.alpha * L_BPNN)
+
+                    epoch_losses.update(loss.item(), len(inputs))
+                    bpnn_loss.update(L_BPNN.item(), len(inputs))
+                    optimizer.zero_grad()
+                    #loss.backward()
+                    loss.mean().backward()
+                    optimizer.step()
+
+                    t.set_postfix(loss='{:.6f}'.format(epoch_losses.avg))
+                    t.update(len(inputs))
+            print("##### Train #####")
+            print("BPNN loss: {:.6f}".format(bpnn_loss.avg))
+            print("train loss : {:.6f}".format(epoch_losses.avg))
+            #torch.save(model.state_dict(), os.path.join(args.outputs_dir, 'epoch_{}.pth'.format(epoch)))
+            tr_score.append(epoch_losses.avg)
+            tr_bpnn.append(bpnn_loss.avg)
+
+            model.eval()
+            epoch_losses_test = AverageMeter()
+            bpnn_loss_test = AverageMeter()
+            for data in eval_dataloader:
                 inputs, labels = data
+
                 inputs = inputs.reshape(inputs.size(0),1,256,256)
                 labels = labels.reshape(labels.size(0),1,512,512)
-                inputs, labels= inputs.float(), labels.float()
+                inputs, labels = inputs.float(), labels.float()
                 inputs = inputs.to(device)
                 labels = labels.to(device)
-                preds = model(inputs)
-                P_SR = model_bpnn(preds)
-                P_HR = model_bpnn(labels)
-                
-                L_SR = criterion(preds, labels)
-                L_BPNN = Lbpnn(P_HR,P_SR)
-                loss = L_SR + (args.alpha * L_BPNN)
-                
-                epoch_losses.update(loss.item(), len(inputs))
-                bpnn_loss.update(L_BPNN.item(), len(inputs))
-                optimizer.zero_grad()
-                #loss.backward()
-                loss.mean().backward()
-                optimizer.step()
 
-                t.set_postfix(loss='{:.6f}'.format(epoch_losses.avg))
-                t.update(len(inputs))
-        print("##### Train #####")
-        print("BPNN loss: {:.6f}".format(bpnn_loss.avg))
-        print("train loss : {:.6f}".format(epoch_losses.avg))
-        #torch.save(model.state_dict(), os.path.join(args.outputs_dir, 'epoch_{}.pth'.format(epoch)))
-        tr_score.append(epoch_losses.avg)
-        tr_bpnn.append(bpnn_loss.avg)
-        
-        model.eval()
-        epoch_losses_test = AverageMeter()
-        bpnn_loss_test = AverageMeter()
-        for data in eval_dataloader:
-            inputs, labels = data
-            
-            inputs = inputs.reshape(inputs.size(0),1,256,256)
-            labels = labels.reshape(labels.size(0),1,512,512)
-            inputs, labels = inputs.float(), labels.float()
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+                with torch.no_grad():
+                    preds = model(inputs).clamp(0.0, 1.0)
+                    Ltest_SR = criterion(preds, labels)
+                    Ltest_BPNN = Lbpnn(P_HR,P_SR)
+                    loss_test = Ltest_SR + (args.alpha * Ltest_BPNN)
+                    epoch_losses_test.update(loss_test.item())
+                    bpnn_loss_test.update(Ltest_BPNN.item())
+            print("##### Test #####")
+            print('eval loss: {:.6f}'.format(epoch_losses_test.avg))
+            print('bpnn loss: {:.6f}'.format(bpnn_loss_test.avg))
+            t_score.append(epoch_losses_test.avg)
+            t_bpnn.append(bpnn_loss_test.avg)
+            if epoch_losses_test.avg < best_loss:
+                best_epoch = epoch
+                best_loss = epoch_losses_test.avg
+                #best_weights = copy.deepcopy(model.state_dict())
+        end = time.time() 
+        print("Time :", end-start) 
+        training_info = {"loss_train": tr_score, "loss_test": t_score, "bpnn_train" : tr_bpnn, "bpnn_test": t_bpnn}
+        with open( os.path.join(args.outputs_dir,"losses_info.pkl"), "wb") as f:
+            pickle.dump(training_info,f)
+        print('best epoch: {}, loss: {:.6f}'.format(best_epoch, best_loss))
+        return best_loss
+        #torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
 
-            with torch.no_grad():
-                preds = model(inputs).clamp(0.0, 1.0)
-                Ltest_SR = criterion(preds, labels)
-                Ltest_BPNN = Lbpnn(P_HR,P_SR)
-                loss_test = Ltest_SR + (args.alpha * Ltest_BPNN)
-                epoch_losses_test.update(loss_test.item())
-                bpnn_loss_test.update(Ltest_BPNN.item())
-        print("##### Test #####")
-        print('eval loss: {:.6f}'.format(epoch_losses_test.avg))
-        print('bpnn loss: {:.6f}'.format(bpnn_loss_test.avg))
-        t_score.append(epoch_losses_test.avg)
-        t_bpnn.append(bpnn_loss_test.avg)
-        if epoch_losses_test.avg < best_loss:
-            best_epoch = epoch
-            best_loss = epoch_losses_test.avg
-            #best_weights = copy.deepcopy(model.state_dict())
-    end = time.time() 
-    print("Time :", end-start) 
-    training_info = {"loss_train": tr_score, "loss_test": t_score, "bpnn_train" : tr_bpnn, "bpnn_test": t_bpnn}
-    with open( os.path.join(args.outputs_dir,"losses_info.pkl"), "wb") as f:
-        pickle.dump(training_info,f)
-    print('best epoch: {}, loss: {:.6f}'.format(best_epoch, best_loss))
-    return best_loss
-    #torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
-    
 study = optuna.create_study(sampler=optuna.samplers.TPESampler(), direction='minimize')
 study.optimize(objective,n_trials=12)
 with open("./FSRCNN_BPNN_search.pkl","wb") as f:
