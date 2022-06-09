2104967f9360d77551db33dd5fa536a3cae52a7d
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-06-09 16:00:41 +0200

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def objective(trial):
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--batch-size', type=int, default=16)
     parser.add_argument('--num-epochs', type=int, default=150)
-    parser.add_argument('--num-workers', type=int, default=8)
+    parser.add_argument('--num-workers', type=int, default=6)
     parser.add_argument('--seed', type=int, default=123)
     parser.add_argument('--nof', type= int, default = 85)
     parser.add_argument('--n1', type=int,default = 158)
