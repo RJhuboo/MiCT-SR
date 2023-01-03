2af0e4fd64619c8ecc90cd5f46574c22bfcc63e2
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2023-01-03 10:25:30 +0100

    BPNN + Mask

--- models.py
+++ models.py
@@ -43,26 +43,27 @@ class FSRCNN(nn.Module):
 class NeuralNet(nn.Module):
     def __init__(self,n1,n2,n3,out_channels):
         super().__init__()
-        self.fc1 = nn.Linear(64*64*64,n1)
+        self.fc1 = nn.Linear((64*64*64)+(512*512),n1)
         self.fc2 = nn.Linear(n1,n2)
         self.fc3 = nn.Linear(n2,n3)
         #self.fc5 = nn.Linear(n3,20)
         self.fc4 = nn.Linear(n3,out_channels)
-    def forward(self,x):
+    def forward(self,mask,x):
+        mask = torch.flatten(mask,1)
         x = torch.flatten(x,1)
+        x = torch.cat((x,mask),1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        #x = F.relu(self.fc5(x))
         x = self.fc4(x)
         return x
     
 ## 3 CNN model ##
 class BPNN(nn.Module):
-    def __init__(self,features,out_channels,n1=240,n2=120,n3=60,k1=3,k2=3,k3=3):
-        super(BPNN,self).__init__()
+    def __init__(self,in_channel,features,out_channels,n1=240,n2=120,n3=60,k1=3,k2=3,k3=3):
+        super(ConvNet,self).__init__()
         # initialize CNN layers 
-        self.conv1 = nn.Conv2d(1,features,kernel_size = k1,stride = 1, padding = 1)
+        self.conv1 = nn.Conv2d(in_channel,features,kernel_size = k1,stride = 1, padding = 1)
         self.conv2 = nn.Conv2d(features,features*2, kernel_size = k2, stride = 1, padding = 1)
         self.conv3 = nn.Conv2d(features*2,64, kernel_size = k3, stride = 1, padding = 1)
         self.pool = nn.MaxPool2d(2,2)
@@ -70,13 +71,16 @@ class BPNN(nn.Module):
         #self.fc1 = nn.Linear(64**3,n1)
         #self.fc2 = nn.Linear(n1,n2)
         #self.fc3 = nn.Linear(n2,14)
+        self.dropout = nn.Dropout2d(0.25)
         self.neural = NeuralNet(n1,n2,n3,out_channels)
         # dropout
         # self.dropout = nn.Dropout(0.25)
-    def forward(self, x):
+    def forward(self,mask, x):
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(F.relu(self.conv2(x)))
+        x= self.dropout(x)
         x = self.pool(F.relu(self.conv3(x)))
-        x = self.neural(x)
+        x = self.neural(mask,x)
         #x = torch.flatten(x,1)
         return x 
