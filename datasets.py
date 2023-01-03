207dc1a7cafa2cb37eea816b7ca8d26884a0d586
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2023-01-03 10:37:44 +0100

    mask added

--- datasets.py
+++ datasets.py
@@ -5,21 +5,24 @@ import os
 from skimage import io
 
 class TrainDataset(Dataset):
-    def __init__(self, HR_dir, LR_dir):
+    def __init__(self, HR_dir, LR_dir, mask_dir):
         super(TrainDataset, self).__init__()
         self.HR_dir = HR_dir
         self.LR_dir = LR_dir
+        self.mask_dir = mask_dir
 
     def __getitem__(self, idx):
         all_images = os.listdir(self.HR_dir)
         HR_path, LR_path = os.path.join(self.HR_dir,all_images[idx]), os.path.join(self.LR_dir,all_images[idx])
         HR = io.imread(HR_path) / 255
         LR = io.imread(LR_path) / 255
-        return LR, HR, all_images[idx]
+        return LR, HR, mask, all_images[idx]
         
     def __len__(self):
         all_images = os.listdir(self.HR_dir)
         return len(all_images)
+    
+    
 
 
 
