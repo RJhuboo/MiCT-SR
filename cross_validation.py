6cd1bdb487b7a395d8ffe3081017fa15bc48cfbe
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-06-09 16:48:59 +0200

    epochs

--- cross_validation.py
+++ cross_validation.py
@@ -60,7 +60,7 @@ def objective(trial):
     parser.add_argument('--scale', type=int, default=4)
     parser.add_argument('--lr', type=float, default=1e-3)#-2
     parser.add_argument('--batch-size', type=int, default=16)
-    parser.add_argument('--num-epochs', type=int, default=1)
+    parser.add_argument('--num-epochs', type=int, default=80)
     parser.add_argument('--num-workers', type=int, default=24)
     parser.add_argument('--seed', type=int, default=123)
     parser.add_argument('--nof', type= int, default = 64)
