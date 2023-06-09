2d65a410723e4a5e7ed9aa025f69b8b2c0a6266a
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-06-09 14:45:13 +0200

    correction

--- cross_validation.py
+++ cross_validation.py
@@ -235,7 +235,7 @@ def objective(trial):
                     #print("prediction:", preds_bin)
                     #if epoch >3:
                        #args.alpha[trial]= 0.0001
-                    loss =  L_SR + args.alpha * L_BPNN
+                    loss =  L_SR + (args.alpha[trial] * L_BPNN)
                     epoch_losses.update(loss.item())
                     bpnn_loss.update(L_BPNN.item())
                     optimizer.zero_grad()
@@ -386,8 +386,8 @@ def objective(trial):
                     #    data_param_SR_test.append(P_SR.detach().numpy())
                     #    names_index_test.append(imagename)
                     Ltest_SR = criterion(preds, labels)
-                    #Ltest_BPNN = Lbpnn(P_SR,P_HR)
-                    Ltest_BPNN = Lbpnn(preds,masks,labels_bin)
+                    Ltest_BPNN = Lbpnn(P_SR,P_HR)
+                    #Ltest_BPNN = Lbpnn(preds,masks,labels_bin)
                     loss_test = Ltest_SR + (args.alpha[trial] * Ltest_BPNN)
                     #for nb_lab in range(args.NB_LABEL):
                     #    L_loss_test[i,nb_lab] = MSE(P_SR[0,nb_lab],P_HR[0,nb_lab],1)
