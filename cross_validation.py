52e39faa507201a78b615740c1579c5827df2b3e
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-06-22 11:35:27 +0200

    Tensorboard track

--- cross_validation.py
+++ cross_validation.py
@@ -6,17 +6,16 @@ import pickle
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torch.nn import L1Loss, MSELoss
+from torch.nn import MSELoss
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
 from torch.utils.data.dataloader import DataLoader
-import torchvision
 import torchvision.transforms as transforms
 from skimage.filters import threshold_otsu 
 import pytorch_ssim
 from tqdm import tqdm
-import optuna
-import joblib
+:qfrom torch.utils.tensorboard import SummaryWriter
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
@@ -25,12 +24,11 @@ from models import FSRCNN, BPNN
 from datasets import TrainDataset, TestDataset
 from utils import AverageMeter, calc_psnr
 from ssim import ssim
-import pandas as pd
 import time
-import sys
-import psutil
 from PIL import Image
-import math
+
+## Create summary for tensorboard
+writer = SummaryWriter(log_dir='runs/'+opt.tensorboard_name)
 
 NB_DATA = 7100
 def bvtv_loss(tensor_to_count,tensor_mask):
@@ -44,12 +42,12 @@ def bvtv_loss(tensor_to_count,tensor_mask):
 
 def objective(trial):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--HR_dir', type=str,default = "./data/HR/Train_Label_trab_100")
-    parser.add_argument('--LR_dir', type=str,default = "./data/LR/Train_trab")
-    parser.add_argument('--mask_dir',type=str,default = "./data/HR/Train_trab_mask")
+    parser.add_argument('--HR_dir', type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/HR/Train_Label_trab_100")
+    parser.add_argument('--LR_dir', type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/LR/Train_trab")
+    parser.add_argument('--mask_dir',type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/HR/Train_trab_mask")
     parser.add_argument('--outputs-dir', type=str, default = "./FSRCNN_search")
     parser.add_argument('--checkpoint_bpnn', type= str, default = "./checkpoints_bpnn/BPNN_checkpoint_lrhr.pth")
-    parser.add_argument('--alpha', type = list, default = [1])
+    parser.add_argument('--alpha', type = list, default = [1e-4])
     parser.add_argument('--Loss_bpnn', default = MSELoss)
     parser.add_argument('--weights-file', type=str)
     parser.add_argument('--scale', type=int, default=2)
@@ -139,7 +137,7 @@ def objective(trial):
         #])
         
         dataset = TrainDataset(args.HR_dir, args.LR_dir, args.mask_dir,transform = my_transforms)
-        dataset_test = TestDataset("../BPNN/Test_trab","./data/LR/Test_trab","../BPNN/Test_trab_mask")
+        dataset_test = TestDataset("/gpfsstore/rech/tvs/uki75tv/Test_trab","/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/LR/Test_trab","/gpfsstore/rech/tvs/uki75tv/Test_trab_mask")
         train_dataloader = DataLoader(dataset=dataset,
                                       batch_size=args.batch_size,
                                       sampler=train_index,
@@ -152,21 +150,20 @@ def objective(trial):
         test_dataloader = DataLoader(dataset=dataset_test,batch_size=1,num_workers=args.num_workers)
         print(len(eval_dataloader))
         print(len(test_dataloader))
-        best_weights = copy.deepcopy(model.state_dict())
-        best_epoch = 0
-        best_loss = 10
-        tr_psnr = []
-        tr_ssim = []
-        e_score, tr_score, tr_bpnn, e_bpnn, e_psnr,e_ssim = [], [] ,[], [], [], []
-        t_score, t_bpnn,t_psnr,t_ssim = [],[],[],[]
+        # best_weights = copy.deepcopy(model.state_dict())
+        # best_epoch = 0
+        # best_loss = 10
+        # tr_psnr, tr_score, tr_bpnn, tr_ssim = [],[], [],[]
+        e_score, e_bpnn, e_psnr,e_ssim = [], [], [], []
+        # t_score, t_bpnn,t_psnr,t_ssim = [],[],[],[]
         #start = time.time()
-        best_epoch_tracking = 1000
-        data_param_SR = []
-        data_param_HR = []
-        data_param_SR_test = []
-        data_param_HR_test = []
-        names_index = []
-        names_index_test = []
+        # best_epoch_tracking = 1000
+        # data_param_SR = []
+        # data_param_HR = []
+        # data_param_SR_test = []
+        # data_param_HR_test = []
+        # names_index = []
+        # names_index_test = []
         for epoch in range(args.num_epochs):
             model.train()
             epoch_losses = AverageMeter()
@@ -253,12 +250,12 @@ def objective(trial):
                     t.set_postfix(loss='{:.9f}'.format(epoch_losses.avg),LossSR='{:.9f}'.format(L_SR.item()),bpnn='{:.3f}'.format(bpnn_loss.avg),psnr='{:.1f}'.format(psnr_train.avg),ssim='{:.1f}'.format(ssim_train.avg),alpha='{:.8f}'.format(args.alpha[trial]))
                     t.update(len(inputs))
                         
-            if epoch > args.num_epochs - 10:
-                torch.save(model.state_dict(), os.path.join(args.outputs_dir, str(args.alpha[trial])+'_best.pth'))
-            tr_psnr.append(psnr_train.avg)
-            tr_ssim.append(ssim_train.avg)
-            tr_score.append(epoch_losses.avg)
-            tr_bpnn.append(bpnn_loss.avg)
+            #if epoch > args.num_epochs - 10:
+                #torch.save(model.state_dict(), os.path.join(args.outputs_dir, str(args.alpha[trial])+'_best.pth'))
+            # tr_psnr.append(psnr_train.avg)
+            # tr_ssim.append(ssim_train.avg)
+            # tr_score.append(epoch_losses.avg)
+            # tr_bpnn.append(bpnn_loss.avg)
             #if epoch==args.num_epochs-1:
             #    df_SR = pd.DataFrame(data_param_SR.reshape(6000,8),index=np.array(names_index).reshape(6000,1))
             #    df_HR = pd.DataFrame(data_param_HR.reshape(6000,8),index=np.array(names_index).reshape(6000,1))
@@ -381,11 +378,11 @@ def objective(trial):
                     Ltest_SR = criterion(preds, labels)
                     Ltest_BPNN = Lbpnn(P_SR,P_HR)
                     loss_test = Ltest_SR + (args.alpha[trial] * Ltest_BPNN)
-                    if epoch == args.num_epochs - 1 :
-                        torchvision.utils.save_image(labels_bin, args.outputs_dir +'/alpha_'+str(args.alpha[trial]) + '/labels_bin_'+imagename[0])
-                        torchvision.utils.save_image(labels, args.outputs_dir +'/alpha_'+str(args.alpha[trial])+'/labels_'+imagename[0])
-                        torchvision.utils.save_image(preds_bin,args.outputs_dir+'/alpha_'+str(args.alpha[trial])+'/preds_bin_'+imagename[0])
-                        torchvision.utils.save_image(preds,args.outputs_dir+'/alpha_'+str(args.alpha[trial])+'/preds'+imagename[0])
+                    #if epoch == args.num_epochs - 1 :
+                    #    torchvision.utils.save_image(labels_bin, args.outputs_dir +'/alpha_'+str(args.alpha[trial]) + '/labels_bin_'+imagename[0])
+                    #    torchvision.utils.save_image(labels, args.outputs_dir +'/alpha_'+str(args.alpha[trial])+'/labels_'+imagename[0])
+                    #    torchvision.utils.save_image(preds_bin,args.outputs_dir+'/alpha_'+str(args.alpha[trial])+'/preds_bin_'+imagename[0])
+                    #    torchvision.utils.save_image(preds,args.outputs_dir+'/alpha_'+str(args.alpha[trial])+'/preds'+imagename[0])
                     
                     epoch_losses_test.update(loss_test.item())
                     bpnn_loss_test.update(Ltest_BPNN.item())
@@ -394,10 +391,16 @@ def objective(trial):
             print("##### TEST #####")
             print("ssim",epoch_losses_test.avg)
             print("psnr", psnr_test.avg)
-            t_score.append(epoch_losses_test.avg)
-            t_bpnn.append(bpnn_loss_test.avg)
-            t_psnr.append(psnr_test.avg)
-            t_ssim.append(ssim_test.avg)
+            # t_score.append(epoch_losses_test.avg)
+            # t_bpnn.append(bpnn_loss_test.avg)
+            # t_psnr.append(psnr_test.avg)
+            # t_ssim.append(ssim_test.avg)
+            
+            writer.add_scalars(str(args.alpha[trial])+'/PSNR',{'train':psnr_train.avg,'val':psnr.avg,'test':psnr_test.avg},epoch)
+            writer.add_scalars(str(args.alpha[trial])+'/Loss MPNN',{'train':bpnn_loss.avg,'test':bpnn_loss_test.avg,'val':bpnn_loss_eval.avg},epoch)
+            writer.add_scalars(str(args.alpha[trial])+'/SSIM',{'train':ssim_train.avg,'test':ssim_test.avg,'val':ssim_list.avg},epoch)
+            writer.add_scalars(str(args.alpha[trial])+'/Loss',{'train':epoch_losses.avg,'test':epoch_losses_test.avg,'val':epoch_losses_eval.avg},epoch)
+
             #df_param_SR_test = pd.DataFrame(np.array(data_param_SR_test).reshape(1100,8),index=np.array(names_index_test).reshape(1100,1))
             #df_param_HR_test = pd.DataFrame(np.array(data_param_HR_test).reshape(1100,8),index=np.array(names_index_test).reshape(1100,1))
 
@@ -427,48 +430,47 @@ def objective(trial):
      #                "train_ssim": cross_ssim_train/args.k_fold,
      #                "train_psnr": cross_psnr_train/args.k_fold
      #               }
-    training_info = {"loss_train": tr_score,
-                     "loss_test": t_score,
-                     "bpnn_train" : tr_bpnn,
-                     "bpnn_test": t_bpnn,
-                     "psnr_test": t_psnr,
-                     "ssim_test": t_ssim,
-                     "train_ssim": tr_ssim,
-                     "train_psnr": tr_psnr,
-                     "eval_psnr": e_psnr,
-                     "eval_ssim": e_ssim,
-                     "bpnn_val": e_bpnn,
-                     "loss_val": e_score,
-                     #"param_SR": df_SR,
-                     #"param_HR": df_HR,
-                     #"param_SR_test": df_param_SR_test,
-                     #"param_HR_test": df_param_HR_test,
-                     "alpha": args.alpha[trial]
-                    }
-    print(" ------------ CROSS RESULTS -------------")
-    print("loss train:",training_info["loss_train"])
-    print("loss val:",training_info['loss_val'])
-    print("loss test:",training_info['loss_test'])
-    print("bpnn train:",training_info["bpnn_train"])
-    print("bpnn_val:", training_info["bpnn_val"])
-    print("alpha:",training_info["alpha"])
-    i=1
-    while os.path.exists(os.path.join(args.outputs_dir,"losses_info"+str(i)+".pkl")) == True:
-        i=i+1
-    with open( os.path.join(args.outputs_dir,"losses_info"+str(i)+".pkl"), "wb") as f:
-        pickle.dump(training_info,f)
+    # training_info = {"loss_train": tr_score,
+    #                  "loss_test": t_score,
+    #                  "bpnn_train" : tr_bpnn,
+    #                  "bpnn_test": t_bpnn,
+    #                  "psnr_test": t_psnr,
+    #                  "ssim_test": t_ssim,
+    #                  "train_ssim": tr_ssim,
+    #                  "train_psnr": tr_psnr,
+    #                  "eval_psnr": e_psnr,
+    #                  "eval_ssim": e_ssim,
+    #                  "bpnn_val": e_bpnn,
+    #                  "loss_val": e_score,
+    #                  "alpha": args.alpha[trial]
+    #                 }
+    # print(" ------------ CROSS RESULTS -------------")
+    # print("loss train:",training_info["loss_train"])
+    # print("loss val:",training_info['loss_val'])
+    # print("loss test:",training_info['loss_test'])
+    # print("bpnn train:",training_info["bpnn_train"])
+    # print("bpnn_val:", training_info["bpnn_val"])
+    # print("alpha:",training_info["alpha"])
+    #i=1
+    #while os.path.exists(os.path.join(args.outputs_dir,"losses_info"+str(i)+".pkl")) == True:
+    #    i=i+1
+    #with open( os.path.join(args.outputs_dir,"losses_info"+str(i)+".pkl"), "wb") as f:
+    #    pickle.dump(training_info,f)
     #print('best epoch: {}, loss: {:.6f}'.format(best_epoch, best_loss))
     #return np.min(np.array(cross_bpnn)/args.k_fold), np.max(np.array(cross_psnr)/args.k_fold), args.alpha[trial], np.max(np.array(cross_ssim)/args.k_fold)
     return np.min(np.array(e_bpnn)),np.max(np.array(e_psnr)),args.alpha[trial],np.max(np.array(e_ssim)),
     #torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
 
-study= {"bpnn" :[], "psnr": [], "alpha": [],"ssim":[]}
-for n_trial in range(2):
+# study= {"bpnn" :[], "psnr": [], "alpha": [],"ssim":[]}
+for n_trial in range(1):
     bp,ps,al,ss = objective(n_trial)
-    study["bpnn"].append(bp)
-    study["psnr"].append(ps)
-    study["alpha"].append(al)
-    study["ssim"].append(ss)
+    # study["bpnn"].append(bp)
+    # study["psnr"].append(ps)
+    # study["alpha"].append(al)
+    # study["ssim"].append(ss)
+    writer.add_scalar('MPNN',bp,al)
+    writer.add_scalar('SSIM',ss,al)
+    writer.add_scalar('PSNR',ps,al)
 
-    with open("BPNN_1.pkl","wb") as f:
-        pickle.dump(study,f)
+    #with open("BPNN_1.pkl","wb") as f:
+    #    pickle.dump(study,f)
