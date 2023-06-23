0684541eea53b7a3aabbed6a0a29159b26e4cb6d
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-06-23 17:51:08 +0200

    rescaling

--- datasets.py
+++ datasets.py
@@ -5,6 +5,7 @@ from skimage import io, transform
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
 import random
+from PIL import Image
 
 class TrainDataset(Dataset):
     def __init__(self, HR_dir, LR_dir, mask_dir,transform=None):
@@ -18,9 +19,15 @@ class TrainDataset(Dataset):
         all_images = os.listdir(self.HR_dir)
         HR_path, LR_path = os.path.join(self.HR_dir,all_images[idx]), os.path.join(self.LR_dir,all_images[idx])
         mask_path = os.path.join(self.mask_dir, all_images[idx].replace(".png",".bmp"))
-        HR = io.imread(HR_path) / 255
-        LR = io.imread(LR_path) / 255
-        mask = io.imread(mask_path) /255
+        HR = Image.open(HR_path)
+        LR = Image.open(LR_path)
+        LR = LR.resize((128,128),Image.BICUBIC)
+        LR = np.array(LR) /255
+        HR = np.array(HR) /255
+        #HR = io.imread(HR_path) / 255
+        #LR = io.imread(LR_path) / 255
+        
+        mask = np.array(Image.open(mask_path)) /255
         #mask = transform.rescale(mask, 1/8, anti_aliasing=False) / 255
         HR = HR.astype('float32')
         LR = LR.astype('float32')
@@ -60,9 +67,14 @@ class TestDataset(Dataset):
         all_images = os.listdir(self.HR_dir)
         HR_path, LR_path = os.path.join(self.HR_dir,all_images[idx]), os.path.join(self.LR_dir,all_images[idx])
         mask_path = os.path.join(self.mask_dir, all_images[idx].replace(".png",".bmp"))
-        HR = io.imread(HR_path) / 255
-        LR = io.imread(LR_path) / 255
-        mask = io.imread(mask_path) / 255
+        HR = np.array(Image.open(HR_path)) /255
+        LR = Image.open(LR_path)
+        LR = LR.resize((128,128),Image.BICUBIC)
+        LR = np.array(LR)/255
+        mask = np.array(Image.open(mask_path))
+        #HR = io.imread(HR_path) / 255
+        #LR = io.imread(LR_path) / 255
+        #mask = io.imread(mask_path) / 255
         HR = HR.astype('float32')
         LR = LR.astype('float32')
         mask = mask.astype('float32')
