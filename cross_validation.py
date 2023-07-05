39cf1d42b8c653902deb18f20046f3cff468f5e2
Rehan JHUBOO <rehan.jhuboo@univ-st-etienne.fr>
2023-07-05 10:14:45 +0200

    x4

--- cross_validation.py
+++ cross_validation.py
@@ -43,7 +43,7 @@ def objective(trial):
     parser.add_argument('--HR_dir', type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/HR/Train_Label_trab_100")
     parser.add_argument('--LR_dir', type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/LR/Train_trab")
     parser.add_argument('--mask_dir',type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/HR/Train_trab_mask")
-    parser.add_argument('--tensorboard_name',type=str,default = "rescale_x2")
+    parser.add_argument('--tensorboard_name',type=str,default = "rescale_x4")
     parser.add_argument('--outputs-dir', type=str, default = "./FSRCNN_search")
     parser.add_argument('--checkpoint_bpnn', type= str, default = "./checkpoints_bpnn/BPNN_checkpoint_TFfsrcnn.pth")
     parser.add_argument('--alpha', type = list, default = [1e-4])
@@ -62,7 +62,7 @@ def objective(trial):
     parser.add_argument('--gpu_ids', type=list, default = [0, 1, 2])
     parser.add_argument('--NB_LABEL', type=int, default = 7)
     parser.add_argument('--k_fold', type=int, default = 1)
-    parser.add_argument('--name', type=str, default = "BPNN_x2")
+    parser.add_argument('--name', type=str, default = "BPNN4_x4")
     args = parser.parse_args()
     
     ## Create summary for tensorboard
