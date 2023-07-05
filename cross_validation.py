e7e9ee0616466854f74ef541286e3c6395d71f85
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-07-05 10:17:50 +0200

    recall

--- cross_validation.py
+++ cross_validation.py
@@ -43,13 +43,13 @@ def objective(trial):
     parser.add_argument('--HR_dir', type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/HR/Train_Label_trab_100")
     parser.add_argument('--LR_dir', type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/LR/Train_trab")
     parser.add_argument('--mask_dir',type=str,default = "/gpfsstore/rech/tvs/uki75tv/data_fsrcnn/HR/Train_trab_mask")
-    parser.add_argument('--tensorboard_name',type=str,default = "rescale_x4")
+    parser.add_argument('--tensorboard_name',type=str,default = "recall_evaluation")
     parser.add_argument('--outputs-dir', type=str, default = "./FSRCNN_search")
     parser.add_argument('--checkpoint_bpnn', type= str, default = "./checkpoints_bpnn/BPNN_checkpoint_TFfsrcnn.pth")
-    parser.add_argument('--alpha', type = list, default = [1e-4])
+    parser.add_argument('--alpha', type = list, default = [0,])
     parser.add_argument('--Loss_bpnn', default = MSELoss)
     parser.add_argument('--weights-file', type=str)
-    parser.add_argument('--scale', type=int, default=4)
+    parser.add_argument('--scale', type=int, default=2)
     parser.add_argument('--lr', type=float, default=1e-3)#-2
     parser.add_argument('--batch-size', type=int, default=16)
     parser.add_argument('--num-epochs', type=int, default=100)
@@ -62,7 +62,7 @@ def objective(trial):
     parser.add_argument('--gpu_ids', type=list, default = [0, 1, 2])
     parser.add_argument('--NB_LABEL', type=int, default = 7)
     parser.add_argument('--k_fold', type=int, default = 1)
-    parser.add_argument('--name', type=str, default = "BPNN4_x4")
+    parser.add_argument('--name', type=str, default = "BPNN_recall_evaluation")
     args = parser.parse_args()
     
     ## Create summary for tensorboard
@@ -433,7 +433,7 @@ def objective(trial):
     #writer.add_scalar('MPNN',np.min(np.array(e_bpnn)),args.alpha[trial])
     #writer.add_scalar('SSIM',np.max(np.array(e_ssim)),args.alpha[trial])
     #writer.add_scalar('PSNR',np.max(np.array(e_psnr)),args.alpha[trial])
-    #torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
+    torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
     return np.min(np.array(e_bpnn)),np.max(np.array(e_psnr)),args.alpha[trial],np.max(np.array(e_ssim)),
 
 # study= {"bpnn" :[], "psnr": [], "alpha": [],"ssim":[]}
