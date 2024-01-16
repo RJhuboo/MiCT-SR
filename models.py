24cd57e38998c5058e00199c41f48d14bd38b286
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2024-01-16 15:23:04 +0100

    Update models.py

--- models.py
+++ models.py
@@ -57,7 +57,6 @@ class NeuralNet(nn.Module):
         self.fc1 = nn.Linear((64*64*64)+(64*64),n1)
         self.fc2 = nn.Linear(n1,n2)
         self.fc3 = nn.Linear(n2,n3)
-        #self.fc5 = nn.Linear(n3,20)
         self.fc4 = nn.Linear(n3,out_channels)
     def forward(self,mask,x):
         mask = torch.flatten(mask,1)
@@ -73,19 +72,13 @@ class NeuralNet(nn.Module):
 class BPNN(nn.Module):
     def __init__(self,in_channel,features,out_channels,n1=240,n2=120,n3=60,k1=3,k2=3,k3=3):
         super(BPNN,self).__init__()
-        # initialize CNN layers 
         self.conv1 = nn.Conv2d(in_channel,features,kernel_size = k1,stride = 1, padding = 1)
         self.conv2 = nn.Conv2d(features,features*2, kernel_size = k2, stride = 1, padding = 1)
         self.conv3 = nn.Conv2d(features*2,64, kernel_size = k3, stride = 1, padding = 1)
         self.pool = nn.MaxPool2d(2,2)
-        # initialize NN layers
-        #self.fc1 = nn.Linear(64**3,n1)
-        #self.fc2 = nn.Linear(n1,n2)
-        #self.fc3 = nn.Linear(n2,14)
         self.dropout = nn.Dropout2d(0.25)
         self.neural = NeuralNet(n1,n2,n3,out_channels)
-        # dropout
-        # self.dropout = nn.Dropout(0.25)
+
     def forward(self,mask, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.dropout(x)
@@ -93,6 +86,4 @@ class BPNN(nn.Module):
         x= self.dropout(x)
         x = self.pool(F.relu(self.conv3(x)))
         x = self.neural(mask,x)
-        #x = self.neural(x)
-        #x = torch.flatten(x,1)
         return x 
