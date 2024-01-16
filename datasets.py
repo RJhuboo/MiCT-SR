ce5f17bcdfa4c598da4df2fabfea4a8bbd75139f
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2024-01-16 15:22:01 +0100

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -8,40 +8,27 @@ import random
 from PIL import Image
 
 class TrainDataset(Dataset):
-    def __init__(self, HR_dir,HR_bin_dir, LR_dir, mask_dir,transform=None):
+    def __init__(self, HR_dir,HR_bin_dir, LR_dir, mask_dir):
         super(TrainDataset, self).__init__()
         self.HR_dir = HR_dir
         self.LR_dir = LR_dir
         self.mask_dir = mask_dir
         self.HR_bin_dir = HR_bin_dir
-        self.transform = transform
 
     def __getitem__(self, idx):
         all_images = os.listdir(self.HR_dir)
         HR_path, LR_path, HR_bin_path = os.path.join(self.HR_dir,all_images[idx]), os.path.join(self.LR_dir,all_images[idx]),os.path.join(self.HR_bin_dir,all_images[idx])
         mask_path = os.path.join(self.mask_dir, all_images[idx].replace(".png",".bmp"))
-<<<<<<< HEAD
-        HR = io.imread(HR_path) / 255
-        HR_bin = io.imread(HR_bin_path) / 255
-        LR = io.imread(LR_path) / 255
-        mask = io.imread(mask_path) /255
-=======
         HR = Image.open(HR_path)
         LR = Image.open(LR_path)
-        LR = LR.resize((128,128))
         LR = np.array(LR) /255
         HR = np.array(HR) /255
-        #HR = io.imread(HR_path) / 255
-        #LR = io.imread(LR_path) / 255
-        
         mask = (np.array(Image.open(mask_path))>0)*1.
->>>>>>> new
-        #mask = transform.rescale(mask, 1/8, anti_aliasing=False) / 255
         HR = HR.astype('float32')
         HR_bin = HR_bin.astype('float32')
         LR = LR.astype('float32')
-        LR = transform.rescale(LR, 1/2, anti_aliasing=True)
         mask = mask.astype('float32')
+        
         p = random.random()
         rot = random.randint(-45,45)
         transform_list = []
@@ -56,18 +43,7 @@ class TrainDataset(Dataset):
         if p>0.2:
             HR,HR_bin,LR,mask=TF.affine(HR,angle=0,translate=(0.1,0.1),shear=0,scale=1),TF.affine(HR_bin,angle=0,translate=(0.1,0.1),shear=0,scale=1),TF.affine(LR,angle=0,translate=(0.1,0.1),shear=0,scale=1),TF.affine(mask,angle=0,translate=(0.1,0.1),shear=0,scale=1)
         HR,HR_bin,LR,mask=TF.to_tensor(HR),TF.to_tensor(HR_bin),TF.to_tensor(LR),TF.to_tensor(mask)
-        #if self.transform:
-        #    HR = self.transform(HR)
-        #    LR = self.transform(LR)
-        #    mask = self.transform(mask)
-<<<<<<< HEAD
-        return LR, HR,HR_bin, mask, all_images[idx]
-=======
-        #print(np.unique(LR))
-        #print(np.shape(LR))
-        #print(LR,HR)
         return LR, HR, mask, all_images[idx]
->>>>>>> new
         
     def __len__(self):
         all_images = os.listdir(self.HR_dir)
@@ -84,26 +60,14 @@ class TestDataset(Dataset):
         all_images = os.listdir(self.HR_dir)
         HR_path,HR_bin_path, LR_path = os.path.join(self.HR_dir,all_images[idx]),os.path.join(self.HR_bin_dir,all_images[idx]), os.path.join(self.LR_dir,all_images[idx])
         mask_path = os.path.join(self.mask_dir, all_images[idx].replace(".png",".bmp"))
-<<<<<<< HEAD
-        HR = io.imread(HR_path) / 255
-        HR_bin = io.imread(HR_bin_path) / 255
-        LR = io.imread(LR_path) / 255
-        mask = io.imread(mask_path) / 255
-=======
         HR = Image.open(HR_path)
         LR = Image.open(LR_path)
-        LR = LR.resize((128,128))
         LR = np.array(LR)/255
         HR = np.array(HR)/255
         mask = np.array(Image.open(mask_path))
-        #HR = io.imread(HR_path) / 255
-        #LR = io.imread(LR_path) / 255
-        #mask = io.imread(mask_path) / 255
->>>>>>> new
         HR = HR.astype('float32')
         HR_bin = HR_bin.astype('float32')
         LR = LR.astype('float32')
-        LR = transform(LR, 1/2, antialiasing=True)
         mask = mask.astype('float32')
         return LR, HR,HR_bin, mask, all_images[idx]
     def __len__(self):
