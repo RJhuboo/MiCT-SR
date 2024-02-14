0c079b23f2c242756376a93251f35f79a645b28a
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2024-02-14 15:11:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -15,10 +15,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights-file', type=str, required=True)
     parser.add_argument('--image-dir', type=str, required=True)
-    parser.add_argument('--label-dir', type=str, required=True)
-    parser.add_argument('--mask-dir', type=str, required=True)
-    parser.add_argument('--output-dir', type=str,required=True)
-    parser.add_argument('--checkpoint_bpnn', type= str, default = "./checkpoints_bpnn/BPNN_checkpoint_12p.pth")
     parser.add_argument('--nof', type= int, default = 23)
     parser.add_argument('--n1', type=int,default = 169)
     parser.add_argument('--n2', type=int,default = 155)
@@ -31,15 +27,6 @@ if __name__ == '__main__':
 
     cudnn.benchmark = True
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    
-    model_bpnn = BPNN(args.nof, args.NB_LABEL, n1= args.n1, n2=args.n2, n3=args.n3, k1=3,k2=3,k3=3).to(device)
-    model_bpnn.load_state_dict(torch.load(os.path.join(args.checkpoint_bpnn)))
-    model_bpnn.to(device)
-    
-    for param in model_bpnn.parameters():
-        param.requires_grad = False
-    model_bpnn.eval()
-    
     model = FSRCNN(scale_factor=args.scale).to(device)
 
     state_dict = model.state_dict()
@@ -50,42 +37,20 @@ if __name__ == '__main__':
             raise KeyError(n)
 
     model.eval()
-
-    psnr = []
-    ssim_list = []
-    mse = []
-## --- TESTING LOOP --- ##
+    
+    ## --- TESTING LOOP --- ##
 
     for image_file in os.listdir(args.image_dir):
         image = pil_image.open(os.path.join(args.image_dir,image_file))
-        image2 = pil_image.open(os.path.join(args.label_dir,image_file))
         image_width = (image.width // args.scale) * args.scale
         image_height = (image.height // args.scale) * args.scale
 
-        #hr = image2.resize((image_width, image_height), resample=pil_image.BICUBIC)
-        hr = image2
         lr = image 
-        #lr = image.resize((hr.width // args.scale, hr.height // args.scale),resample=pil_image.BICUBIC)
        
         lr = preprocess(lr, device)
-        hr= preprocess(hr, device)
-        
-        #savelr = preprocess(image,device)
-
         with torch.no_grad():
             preds = model(lr).clamp(0.0, 1.0)
         
-            P_SR = model_bpnn(preds)
-            P_HR = model_bpnn(hr)
-            Loss = MSELoss()
-            mse.append(Loss(P_SR,P_HR).item())
-        
-        print(hr.size())
-        print(preds.size())
-        psnr.append(calc_psnr(hr, preds, args.mask_dir, image_file,device))
-        ssim_list.append(ssim(x=hr, y=preds, data_range=1., downsample = False, directory=args.mask_dir, maskname=image_file,device=device))
-        #psnr2.append(calc_psnr(hr,savelr))
-
         preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
 
         output = np.array(preds)
@@ -94,9 +59,3 @@ if __name__ == '__main__':
         directory = args.output_dir
         name_save = image_file  
         output.save(os.path.join(directory, name_save))
-
-    print('PSNR COMPUTATION ...')
-    print('Average PSNR SR :', sum(psnr)/len(psnr))
-    print('Average MSE :', sum(mse)/len(mse))
-    print('Average SSIM SR :', sum(ssim_list)/len(ssim_list))
-    #print('Average PSNR LR : ', sum(psnr2)/len(psnr2))
